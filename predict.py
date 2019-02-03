4696e12c104d3a3b2b93a887aec27d2b03f7b854
sho <hira.alpherz@gmail.com>
2019-02-03 13:02:04 +0900

    predict

--- predict.py
+++ predict.py
@@ -15,6 +15,19 @@ test_dir = 'data/test/'
 input_size = 224
 seed = 1470
 
+def load_data():
+    datagen = ImageDataGenerator(
+       rescale=1./255,
+    )
+    train_generator = datagen.flow_from_directory(
+        'data/train',
+        target_size=(input_size, input_size),
+        batch_size=1,
+        shuffle=False,
+        seed=seed,
+    )
+    return train_generator
+
 def load_test_data():
     datagen = ImageDataGenerator(
         rescale=1./255,
@@ -29,14 +42,51 @@ def load_test_data():
     )
     return test_generator
 
+def save_class():
+    train_generator = load_data()
+    classes= train_generator.class_indices
+    df = pd.DataFrame.from_dict(classes, orient='index')
+    df.to_csv('train_classes_classify.cls')
+
+def predict(new_known_model, classify_model, generator, steps):
+    num = 5
+    generator.reset()
+    pred_new = new_known_model.predict_generator(generator, steps=steps, verbose=1)
+    generator.reset()
+    pred_classify = classify_model.predict_generator(generator, steps=steps, verbose=1)
+    classify_index = np.argpartition(-pred_classify, num)[:, :num]
+    classify_value = pred_classify[np.arange(pred_classify.shape[0])[:, None], classify_index]
+    # create output
+    df = pd.read_csv('train_classes_classify.cls')
+    classes = df.loc[:, 'Unnamed: 0']
+    submit = pd.DataFrame(columns=['Image', 'Id'])
+    for i, pred in enumerate(pred_new):
+        sort_index = np.argsort(classify_value[i])[::-1]
+        classify_index[i] = classify_index[i][sort_index]
+        classify_value[i] = classify_value[i][sort_index]
+        submit_classes = []
+        # if pred[0] > 0.9999 and classify_value[i][0] < 0.5:
+        #     submit_classes.append('new_whale')
+        #     submit_classes.extend(classes[classify_index[i]].values[0:4])
+        # elif pred[0] > 0.9:
+        #     submit_classes.extend(classes[classify_index[i]].values[0:4])
+        #     submit_classes.append('new_whale')
+        # else:
+        submit_classes.extend(classes[classify_index[i]].values[0:5])
+        classes_text = ' '.join(submit_classes)
+        submit = submit.append(pd.Series(np.array([generator.filenames[i].split('/')[1], classes_text]), index=submit.columns), ignore_index=True)
+    return submit
+
+# load models
 new_known_model = models.load_model('models/separate1_new_known/model.h5')
 classify_model = models.load_model('models/separate2_classify/115-4.96-4.36.hdf5')
-
-new_known_model.summary()
-classify_model.summary()
-
+# load test data
 test_generator = load_test_data()
+train_generator = load_data()
+# predict
+submit = predict(new_known_model, classify_model, test_generator, steps=len(test_generator))
 
-test_generator.reset()
-pred = new_known_model.predict_generator(test_generator, steps=len(test_generator),verbose=1)
-pred = classify_model(test_generator, verbose=1)
+submit.to_csv('submit.csv', index=False)
+
+
+
